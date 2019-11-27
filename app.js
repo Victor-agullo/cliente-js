6d7f66000b1aca44c082429085a140b4576ad0bd
Eztena <victor_jesus_agullo_apps1ma1819@cev.com>
2019-11-27 14:38:01 +0100

    Client operative.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+window.onload = buttonPressed;
+
 function buttonPressed() {
-    document.getElementById("submit").onclick = poster();
+    document.getElementById("submit").onclick = poster;
 }
 
 function poster() {
-    var obtainedName = document.getElementById("name");
-    var mail = document.getElementById("email");
-    var pass = document.getElementById("password");
+    var obtainedName = document.getElementById("name").value;
+    var mail = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
     var obj = {"name": obtainedName, "email": mail, "password": pass};
     $.ajax({
-        type:"POST", 
-        url:"http://localhost:8888/client/",
-        data:obj,
-        success: function k (response) {
-            $("$resoult").html(response);
+        type: "POST", 
+        url: "http://localhost:8888/library/public/index.php/api/users",
+        data: obj,
+        success: function (response) {
+            $("$resultado").html(response);
         }
     });
 }
